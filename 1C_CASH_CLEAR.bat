88dfa21fe6ac2c66aee13f798cddcb9bffbf6f5d
vav <toni_vasilenko@mail.ru>
2020-09-28 18:18:42 +0600

    initial commit

---
+++ 1C_CASH_CLEAR.bat
@@ -0,0 +1,9 @@
+@echo off
+del     /s /q %userprofile%\AppData\Local\1C\*.*  
+rd      /s /q %userprofile%\AppData\Local\1C\
+ 
+del     /s /q %userprofile%\AppData\Roaming\1C\1cv8\*.*
+rd      /s /q %userprofile%\AppData\Roaming\1C\1cv8\
+
+del     /s /q %userprofile%\AppData\Roaming\1C\1cv82\*.*
+rd      /s /q %userprofile%\AppData\Roaming\1C\1cv82\ 
---
+++ win10_return_shortcuts.bat
@@ -0,0 +1,4 @@
+taskkill /IM explorer.exe /F 
+cd /d %userprofile%\AppData\Local
+del IconCache.db /a
+start "" explorer.exe
